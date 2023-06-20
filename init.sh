e18a94e423c0e79cc7f5dd799da112d6eb43427c
Christopher Speller <crspeller@gmail.com>
2023-06-20 06:14:02 -0700

    mkdir

--- init.sh
+++ init.sh
@@ -12,6 +12,7 @@ echo -e "Setting up Mattermost with ...\n Team name: $team_name\n Team display n
 echo "Initializing Mattermost for demo. This will take about 30 seconds..."
 
 # Download sample local model
+mkdir -p models
 wget https://gpt4all.io/models/ggml-gpt4all-j.bin -O models/ggml-gpt4all-j &
 echo "The prompt below is a question to answer, a task to complete, or a conversation to respond to; decide which and write an appropriate response.\n### Prompt:\n{{.Input}}\n### Response:" > models/ggml-gpt4all-j.tmpl
 
